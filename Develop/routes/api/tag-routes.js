83e142363ece5636361f054b7885838055af8087
Gregory Friel <87154134+Friel2021@users.noreply.github.com>
2021-09-08 15:42:56 -0600

    routes added

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,104 +4,82 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-  try {
-    const tagData = await Tag.findAll({
-      // attributes: ['tag_name'],
-      include: [
-        {
-          model: Product,
-          attributes: ['product_name', 'price', 'stock']
-        },
-      ],
-    });
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	// find all tags
+	// be sure to include its associated Product data
+	try {
+		const tagData = await Tag.findAll({
+			include: [{ model: Product, through: ProductTag, as: 'products_with_tag' }],
+		});
+		res.status(200).json(tagData);
+	} catch (error) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-  try {
-    const tagData = await Tag.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [{
-        model: Product,
-        attributes: ['product_name', 'price', 'stock']
-      }]
-    });
-
-    if (!tagData) {
-      res.status(404).json({
-        message: 'No tag found with this id!'
-      });
-      return;
-    }
-
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	// find a single tag by its `id`
+	// be sure to include its associated Product data
+	try {
+		const tagData = await Tag.findByPk(req.params.id, {
+			include: [{ model: Product, through: ProductTag, as: 'products_with_tag' }],
+		});
+		if (!tagData) {
+			res.status(404).json({ message: 'No tag found with that id!' });
+			return;
+		}
+		res.status(200).json(tagData);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
-  try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+	// create a new tag
+	try {
+		const tagData = await Tag.create(req.body);
+		res.status(200).json(tagData);
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
-// UPDATE TAG not working correctly.
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
-  try {
-    const tagData = await Tag.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    });
-
-    if (!tagData) {
-      res.status(404).json({
-        message: 'No tag found with this id!'
-      });
-      return;
-    }
-
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	// update a tag's name by its `id` value
+	try {
+		const tagData = await Tag.update(req.body, {
+			where: {
+				id: req.params.id,
+			},
+		});
+		if (!tagData) {
+			res.status(404).json({ message: 'No tag found with that id!' });
+			return;
+		}
+		res.status(200).json(tagData);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
-  try {
-    const tagData = await Tag.destroy({
-      where: {
-        id: req.params.id
-      }
-    });
+	// delete on tag by its `id` value
+	try {
+		const tagData = await Tag.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
 
-    if (!tagData) {
-      res.status(404).json({
-        message: 'No tag found with this id!'
-      });
-      return;
-    }
+		if (!tagData) {
+			res.status(404).json({ message: 'No tags found with that id!' });
+			return;
+		}
+		res.status(200).json(tagData);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	res.status(200).json(tagData);
 });
 
 module.exports = router;
